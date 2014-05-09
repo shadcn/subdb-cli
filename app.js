08ad49af60e70fb6f144b910722c3d8a7d601ec4
Arshad Chummun <arshad@chummun.com>
2014-05-10 00:17:19 +0400

    Turned this into a cli command

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
+#! /usr/bin/env node
+var program = require('commander');
 var Subdb = require('./lib/subdb.js');
-var path = '/Users/arshad/Desktop/dexter.mp4';
 
-Subdb.download(path);
+// Get version from package.json.
+var version = require('./package.json').version;
+
+program
+   .version(version)
+
+/**
+ * Download command.
+ * subdb download [file].
+ */
+program
+  .command('download [file]')
+  .description('Use this command to download subtitle for a file.')
+  .action(function(file, options){
+    if (!file) {
+      console.log('Error: file name missing');
+      program.help();
+    }
+
+    // Download the subtitle.
+    Subdb.download(file);
+  });
+
+// Examples.
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ subdb download video.avi');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
